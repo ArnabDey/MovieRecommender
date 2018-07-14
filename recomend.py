020faa1da7afa8c627b8f991394b85f01a85efab
arnabdey <arnabdey29@gmail.com>
2018-07-14 13:11:22 -0400

    Merge datasets for easy data analysis

--- recomend.py
+++ recomend.py
@@ -2,25 +2,26 @@ import numpy as np
 import sklearn
 import pandas as pd
 import matplotlib
+import csv
 
 movies = pd.read_csv('./ml-latest-small/movies.csv')
 tags  = pd.read_csv('./ml-latest-small/tags.csv')
 ratings = pd.read_csv('./ml-latest-small/ratings.csv')
 
-def findById(movieId):
-    for index, row in movies.iterrows():
-        if (row['movieId'] == movieId):
-            return row['title']
+# Finds sentiment for the movie using NLP
+def findSentiment():
+    import re
+    import nltk
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+    from nltk.stem.porter import PorterStemmer
+    # Cleaning the dataset
+    print(tags)
 
-def findRatings(movieId):
-    ratingArr = []
-    for index, row in ratings.iterrows():
-        if (row['movieId'] == movieId):
-            ratingArr.append(row['rating'])
-    sum = 0
-    for val in ratingArr:
-        sum = sum + val
-    return sum * 1.0 / len(ratingArr)
+# Organizing the Data
+averageRatings = ratings.groupby(['movieId'], as_index = False).mean()
+combinedTags = tags.groupby('movieId')['tag'].apply(list).reset_index()
+movieRating = pd.merge(averageRatings, movies, on = "movieId", how = "outer").fillna("")
 
-print(findById(2))
-print(str(findRatings(2)/ 5 * 100)  + '% precent of people liked the movie')
+combinedData = pd.merge(movieRating, combinedTags, on = "movieId", how = "outer").fillna("")
+combinedData.to_csv('mergedata.csv', index = False)
